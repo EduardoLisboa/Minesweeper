1b8a2016a1c20a03548ae27e8bf0c6d5e44cd49e
Eduardo Antônio de Lucena Lisboa <38367195+EduardoLisboa@users.noreply.github.com>
2021-02-03 18:05:27 -0300

    First commit
    First commit of the complete game.

---
+++ Assets/images.py
@@ -0,0 +1,47 @@
+import pygame
+import os
+from Constants.constants import SQUARE_SIZE
+
+BOMB_ICON = pygame.image.load(os.path.join('Assets', 'PNGs', 'bomb_icon.png'))
+
+BOMB_IMAGE = pygame.image.load(os.path.join('Assets', 'PNGs', 'bomb.png'))
+BOMB = pygame.transform.scale(BOMB_IMAGE, (SQUARE_SIZE - 5, SQUARE_SIZE - 5))
+
+FLAG_IMAGE = pygame.image.load(os.path.join('Assets', 'PNGs', 'flag.png'))
+FLAG = pygame.transform.scale(FLAG_IMAGE, (SQUARE_SIZE - 5, SQUARE_SIZE - 5))
+
+EMPTY_IMAGE = pygame.image.load(os.path.join('Assets', 'PNGs', 'square.png'))
+EMPTY = pygame.transform.scale(EMPTY_IMAGE, (SQUARE_SIZE - 5, SQUARE_SIZE - 5))
+
+ONE_IMAGE = pygame.image.load(os.path.join('Assets', 'PNGs', '1.png'))
+ONE = pygame.transform.scale(ONE_IMAGE, (SQUARE_SIZE - 5, SQUARE_SIZE - 5))
+
+TWO_IMAGE = pygame.image.load(os.path.join('Assets', 'PNGs', '2.png'))
+TWO = pygame.transform.scale(TWO_IMAGE, (SQUARE_SIZE - 5, SQUARE_SIZE - 5))
+
+THREE_IMAGE = pygame.image.load(os.path.join('Assets', 'PNGs', '3.png'))
+THREE = pygame.transform.scale(THREE_IMAGE, (SQUARE_SIZE - 5, SQUARE_SIZE - 5))
+
+FOUR_IMAGE = pygame.image.load(os.path.join('Assets', 'PNGs', '4.png'))
+FOUR = pygame.transform.scale(FOUR_IMAGE, (SQUARE_SIZE - 5, SQUARE_SIZE - 5))
+
+FIVE_IMAGE = pygame.image.load(os.path.join('Assets', 'PNGs', '5.png'))
+FIVE = pygame.transform.scale(FIVE_IMAGE, (SQUARE_SIZE - 5, SQUARE_SIZE - 5))
+
+SIX_IMAGE = pygame.image.load(os.path.join('Assets', 'PNGs', '6.png'))
+SIX = pygame.transform.scale(SIX_IMAGE, (SQUARE_SIZE - 5, SQUARE_SIZE - 5))
+
+SEVEN_IMAGE = pygame.image.load(os.path.join('Assets', 'PNGs', '7.png'))
+SEVEN = pygame.transform.scale(SEVEN_IMAGE, (SQUARE_SIZE - 5, SQUARE_SIZE - 5))
+
+EIGHT_IMAGE = pygame.image.load(os.path.join('Assets', 'PNGs', '8.png'))
+EIGHT = pygame.transform.scale(EIGHT_IMAGE, (SQUARE_SIZE - 5, SQUARE_SIZE - 5))
+
+numbers_dict = {1 : ONE,
+                2 : TWO,
+                3 : THREE,
+                4 : FOUR,
+                5 : FIVE,
+                6 : SIX,
+                7 : SEVEN,
+                8 : EIGHT}
---
+++ main.py
@@ -0,0 +1,192 @@
+import pygame
+from Constants.constants import WIDTH, HEIGHT, BLACK, WHITE, DARK_GREY, SQUARE_SIZE, FPS
+from Assets.images import BOMB_ICON, BOMB, FLAG, EMPTY, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, numbers_dict
+from random import randint
+pygame.font.init()
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Campo Minado")
+pygame.display.set_icon(BOMB_ICON)
+
+TEXT_FONT = pygame.font.SysFont('calibri', 150)
+
+
+def make_table():
+    WIN.fill(BLACK)
+    for i in range(SQUARE_SIZE, WIDTH, SQUARE_SIZE):
+        vertical = pygame.Rect(i, 0, 2, HEIGHT)
+        horizontal = pygame.Rect(0, i, WIDTH, 2)
+        pygame.draw.rect(WIN, DARK_GREY, vertical)
+        pygame.draw.rect(WIN, DARK_GREY, horizontal)
+
+
+def is_bomb(mouse_x, mouse_y, bombs):
+    if (mouse_x, mouse_y) in bombs:
+        return True
+    else:
+        return False
+
+
+def check_bombs(mouse_x, mouse_y, bombs):
+    cont = 0
+    for col in range(-1, 2):
+        for row in range(-1, 2):
+            if col == 0 and row == 0 or mouse_x < 0 or mouse_x > 19 or mouse_y < 0 or mouse_y > 19:
+                continue
+            if (mouse_x + col, mouse_y + row) in bombs:
+                cont += 1
+
+    return cont
+
+
+def check_empty_spaces(mouse_x, mouse_y, bombs, empty_spaces, visited, not_bomb):
+    if (mouse_x, mouse_y) in visited:
+        return
+    num_bombs = check_bombs(mouse_x, mouse_y, bombs)
+    if num_bombs == 0 and (mouse_x, mouse_y) not in visited:
+        if (mouse_x, mouse_y) not in not_bomb and (mouse_x, mouse_y) not in bombs and mouse_x >= 0 and mouse_x <= 19 and mouse_y >= 0 and mouse_y <= 19:
+            not_bomb.append((mouse_x, mouse_y))
+        empty_spaces.append([(mouse_x, mouse_y), 0])
+    elif num_bombs > 0 and (mouse_x, mouse_y) not in visited:
+        if (mouse_x, mouse_y) not in not_bomb and (mouse_x, mouse_y) not in bombs and mouse_x >= 0 and mouse_x <= 19 and mouse_y >= 0 and mouse_y <= 19:
+            not_bomb.append((mouse_x, mouse_y))
+        empty_spaces.append([(mouse_x, mouse_y), num_bombs])
+        return
+    
+    visited.append((mouse_x, mouse_y))
+
+    for col in range(-1, 2):
+        for row in range(-1, 2):
+            if col == 0 and row == 0 or mouse_x < 0 or mouse_x > 19 or mouse_y < 0 or mouse_y > 19:
+                continue
+            check_empty_spaces(mouse_x + col, mouse_y + row, bombs, empty_spaces, visited, not_bomb)
+
+
+def create_bombs():
+    bombs = list()
+    bombs.clear()
+    col = randint(0, 19)
+    row = randint(0, 19)
+    for _ in range(0, 80):
+        while (col, row) in bombs:
+            col = randint(0, 19)
+            row = randint(0, 19)
+        bombs.append((col, row))
+
+    no_bombs = list()
+    no_bombs.clear()
+    for col in range(0, 20):
+        for row in range(0, 20):
+            if (col, row) not in bombs:
+                no_bombs.append((col, row))
+
+    return bombs, no_bombs
+
+
+def draw_bombs(bombs, draw_as_flag):
+    for bomb in bombs:
+        if draw_as_flag:
+            black_square = pygame.Rect(bomb[0] * SQUARE_SIZE + 2, bomb[1] * SQUARE_SIZE + 2, SQUARE_SIZE - 3, SQUARE_SIZE - 3)
+            pygame.draw.rect(WIN, BLACK, black_square)
+            WIN.blit(FLAG, (bomb[0] * SQUARE_SIZE + 3, bomb[1] * SQUARE_SIZE + 3))
+        else:
+            black_square = pygame.Rect(bomb[0] * SQUARE_SIZE + 2, bomb[1] * SQUARE_SIZE + 2, SQUARE_SIZE - 3, SQUARE_SIZE - 3)
+            pygame.draw.rect(WIN, BLACK, black_square)
+            WIN.blit(BOMB, (bomb[0] * SQUARE_SIZE + 3, bomb[1] * SQUARE_SIZE + 3))
+
+
+def draw_number(mouse_x, mouse_y, num_bombs):
+    WIN.blit(numbers_dict[num_bombs], (mouse_x * SQUARE_SIZE + 3, mouse_y * SQUARE_SIZE + 3))
+
+
+def draw_empty_spaces(empty_spaces):
+    for space in empty_spaces:
+        if space[1] == 0:
+            WIN.blit(EMPTY, (space[0][0] * SQUARE_SIZE + 3, space[0][1] * SQUARE_SIZE + 3))
+        else:
+            draw_number(space[0][0], space[0][1], space[1])
+
+
+def print_text(text):
+    draw_text = TEXT_FONT.render(text, 1, WHITE)
+    black_rect = pygame.Rect(0, HEIGHT // 2 - draw_text.get_height() // 2 - 20, WIDTH, draw_text.get_height() + 30)
+    pygame.draw.rect(WIN, BLACK, black_rect)
+    WIN.blit(draw_text, (WIDTH // 2 - draw_text.get_width() // 2, HEIGHT // 2 - draw_text.get_height() // 2))
+
+
+def main():
+    run = True
+    
+    make_table()
+
+    bombs, no_bombs = create_bombs()
+    not_bomb = list()
+
+    pressed = list()
+    numbers = list()
+    empty_spaces = list()
+    flags = list()
+    visited = list()
+
+    clock = pygame.time.Clock()
+    
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+            
+                pos = pygame.mouse.get_pos()
+                mouse_x, mouse_y = pos[0] // SQUARE_SIZE, pos[1] // SQUARE_SIZE
+                button = pygame.mouse.get_pressed()
+
+                if button == (1, 0, 0) and (mouse_x, mouse_y) not in pressed:
+                    pressed.append((mouse_x, mouse_y))
+
+                    if is_bomb(mouse_x, mouse_y, bombs):
+                        draw_bombs(bombs, False)
+                        print_text('YOU LOSE!')
+                        pygame.display.update()
+                        pygame.time.delay(5000)
+                        main()
+                    else:
+                        num_bombs = check_bombs(mouse_x, mouse_y, bombs)
+                        if num_bombs > 0:
+                            draw_number(mouse_x, mouse_y, num_bombs)
+                            numbers.append((mouse_x, mouse_y))
+                            not_bomb.append((mouse_x, mouse_y))
+                        else: # EMPTY SPACE
+                            check_empty_spaces(mouse_x, mouse_y, bombs, empty_spaces, visited, not_bomb)
+                            draw_empty_spaces(empty_spaces)
+                            empty_spaces.clear()
+                if button == (0, 0, 1):
+                    if (mouse_x, mouse_y) not in pressed:
+                        pressed.append((mouse_x, mouse_y))
+                        black_square = pygame.Rect(mouse_x * SQUARE_SIZE + 2, mouse_y * SQUARE_SIZE + 2, SQUARE_SIZE - 3, SQUARE_SIZE - 3)
+                        pygame.draw.rect(WIN, BLACK, black_square)
+                        WIN.blit(FLAG, (mouse_x * SQUARE_SIZE + 3, mouse_y * SQUARE_SIZE + 3))
+                        flags.append((mouse_x, mouse_y))
+                    elif (mouse_x, mouse_y) in pressed and (mouse_x, mouse_y) in flags:
+                        flags.remove((mouse_x, mouse_y))
+                        pressed.remove((mouse_x, mouse_y))
+                        black_square = pygame.Rect(mouse_x * SQUARE_SIZE + 2, mouse_y * SQUARE_SIZE + 2, SQUARE_SIZE - 3, SQUARE_SIZE - 3)
+                        pygame.draw.rect(WIN, BLACK, black_square)
+
+        if len(not_bomb) == len(no_bombs):
+            draw_bombs(bombs, True)
+            print_text('YOU WIN!')
+            pygame.display.update()
+            pygame.time.delay(5000)
+            main()
+
+        draw_bombs(bombs, False)
+        pygame.display.update()
+    
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
