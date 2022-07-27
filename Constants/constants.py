ae2d0b73b63108cc38223994f090e1d6aa238e0b
Eduardo Antônio de Lucena Lisboa <eall@ic.ufal.br>
2022-07-27 16:12:38 -0300

    Refactor constants file
    Refactor constants file to propperly calculate the tile sizes

--- Constants/constants.py
+++ Constants/constants.py
@@ -1,15 +1,16 @@
 import pygame
 pygame.font.init()
 
-TEXT_FONT = pygame.font.SysFont('calibri', 150)
+WIDTH, HEIGHT = 600, 600
 
-WIDTH, HEIGHT = 800, 800
+TEXT_FONT = pygame.font.SysFont('calibri', WIDTH//5)
+BOMB_QTD_FONT = pygame.font.SysFont('calibri', 45)
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 DARK_GREY = (70, 70, 70)
 
-SQUARE_SIZE = 40
+SQUARE_SIZE = WIDTH // 20
 
 QTD_BOMBS = 60
 
